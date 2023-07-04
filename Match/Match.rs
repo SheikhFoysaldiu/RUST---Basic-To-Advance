7265d162f3060732bdfd0840450ad7dc61c70901
sheikhfoysaldiu <foysal15-3442@diu.edu.bd>
2023-07-04 11:35:04 +0600

    Match

---
+++ Match/Match.rs
@@ -0,0 +1,21 @@
+
+
+fn main()
+{
+    let x = 10;
+    match x
+    {
+        1 => println!("x is one"),
+        2 => println!("x is two"),
+        3 => println!("x is three"),
+        4 => println!("x is four"),
+        5 => println!("x is five"),
+        6 => println!("x is six"),
+        7 => println!("x is seven"),
+        8 => println!("x is eight"),
+        9 => println!("x is nine"),
+        10 => println!("x is ten"),
+        _ => println!("x is unknown"), // 
+    }
+
+}
