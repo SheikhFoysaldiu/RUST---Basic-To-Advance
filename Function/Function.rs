7a3a9a34474d5646f97d03c31b930e9648a9e1af
sheikhfoysaldiu <foysal15-3442@diu.edu.bd>
2023-07-04 11:24:15 +0600

    Fucntion

---
+++ Function/Function.rs
@@ -0,0 +1,26 @@
+// Topic: Functions
+//
+// Program requirements:
+// * Displays your first and last name
+//
+// Notes:
+// * Use a function to display your first name
+// * Use a function to display your last name
+// * Use the println macro to display messages to the terminal
+
+
+fn first_name()
+{
+    println!("My first name is: {}", "Foysal");
+}
+
+fn last_name()
+{
+    println!("My last name is: {}", "Sheikh");
+}
+
+fn main() {
+    first_name();
+    last_name();
+
+}
