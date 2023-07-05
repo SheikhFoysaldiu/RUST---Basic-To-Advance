678a8d69eff997777ae8fb97e6f6922731df6c33
SheikhFoysaldiu <foysal15-3442@diu.edu.bd>
2023-07-05 23:34:08 +0600

    Enum-2.0

--- Enum/Enum.rs
+++ Enum/Enum.rs
@@ -1,3 +1,6 @@
+// The enum keyword allows the creation of a type which may be one of a few different variants. 
+// Any variant which is valid as a struct is also valid in an enum.
+
 enum Direction
 {
     Up,
