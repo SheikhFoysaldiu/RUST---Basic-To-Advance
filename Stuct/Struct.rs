c66fb2a99786dd7db9a499fa79f86cb584280594
SheikhFoysaldiu <foysal15-3442@diu.edu.bd>
2023-07-05 20:07:08 +0600

    Tuple

---
+++ Stuct/Struct.rs
@@ -0,0 +1,17 @@
+struct Product {
+    name: string,
+    price : i32,
+    stock: i32
+
+}
+
+fn main()
+{
+    let Item = Product{
+        name: "Biscuit",
+        price: 32,
+        stock : 3
+    }
+
+    println!({},Item.name)
+}
---
+++ Tuple/Tuple.rs
@@ -0,0 +1,13 @@
+fn main()
+{  
+    let cord = (1,3);
+    println!("FirstNumber {} SecondNumber {}", cord.0,cord.1);
+    
+    let favorite = (1,"2",3.3,'a');
+    println!("{} {} {} {}",favorite.0,favorite.1,favorite.2,favorite.3);
+
+}
+
+//Output:
+// FirstNumber 1 SecondNumber 3
+// 1 2 3.3 a
