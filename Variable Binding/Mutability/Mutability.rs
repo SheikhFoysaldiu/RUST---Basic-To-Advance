197c1f59528ac79d4b52d745a5785a3f4cbe5ba2
SheikhFoysaldiu <foysal15-3442@diu.edu.bd>
2023-07-05 23:18:34 +0600

    Varibale Binding - Freezing, Mutability

---
+++ Variable Binding/Mutability/Mutability.rs
@@ -0,0 +1,20 @@
+// Variable bindings are immutable by default, but this can be overridden using the mut modifier.
+
+fn main() {
+    let _immutable_binding = 1;
+    let mut mutable_binding = 1;
+
+    println!("Before mutation: {}", mutable_binding);
+
+    // Ok
+    mutable_binding += 1;
+
+    println!("After mutation: {}", mutable_binding);
+
+    // Error! Cannot assign a new value to an immutable variable
+    // _immutable_binding += 1;
+}
+
+// output:
+// Before mutation: 1
+// After mutation: 2
